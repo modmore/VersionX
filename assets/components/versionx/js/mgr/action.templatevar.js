08bd1ea6c59b30172131db3e87e33c45b56cf5e4
Adam Lundrigan <adam@lundrigan.ca>
2012-01-24 11:21:53 -0330

    Added "VersionX TEmplate Variable Details" page (minus the "properties" tab)

---
+++ assets/components/versionx/js/mgr/action.templatevar.js
@@ -0,0 +1,50 @@
+
+Ext.onReady(function() {
+    Ext.QuickTips.init();
+    page = MODx.load({ xtype: 'versionx-page-templatevars'});
+    page.show();
+});
+
+VersionX.page.Template = function(config) {
+    config = config || {};
+    var buttons = [];
+    buttons.push({
+        text: _('versionx.back'),
+        handler: function () {
+            window.location.href = '?a='+MODx.request['a'];
+        }
+    });
+    if (MODx.request.backTo) {
+        var back = MODx.request.backTo.split('-');
+        if (back[0] == MODx.action['templatevars/update']) {
+            buttons.push('-',{
+                text: _('versionx.backtotemplatevars'),
+                handler: function() {
+                    window.location.href = '?a='+back[0]+'&id='+back[1];
+                }
+            });
+        }
+    }
+    Ext.applyIf(config,{
+        renderTo: 'versionx',
+        cls: 'container',
+        components: [{
+            xtype: 'panel',
+            html: '<h2>'+_('versionx')+' '+_('versionx.templatevars.detail')+'</h2>',
+            cls: 'modx-page-header',
+            border: false
+        },{
+            xtype: 'versionx-panel-templatevarsdetail',
+            cls: 'x-panel-body',
+            vxRecord: VersionX.record,
+            vxRecordCmp: VersionX.cmrecord,
+            border: false,
+            width: '98%'
+        }],
+        buttons: buttons
+    });
+    VersionX.page.Template.superclass.constructor.call(this,config);
+};
+Ext.extend(VersionX.page.Template,MODx.Component);
+Ext.reg('versionx-page-templatevars',VersionX.page.Template);
+
--- core/components/versionx/controllers/index.php
+++ core/components/versionx/controllers/index.php
@@ -98,6 +98,35 @@ switch ($_REQUEST['action']) {
         $modx->regClientStartupScript($versionx->config['js_url'].'mgr/templates/combo.versions.templates.js'); 
     break;
     
+    case 'templatevar':
+        /* If an ID was passed, fetch that version into a record array. */
+        if (intval($_REQUEST['vid']) > 0) {
+            $v = $versionx->getVersionDetails('vxTemplateVar',intval($_REQUEST['vid']));
+            if ($v !== false)
+                $modx->regClientStartupHTMLBlock('
+                    <script type="text/javascript">VersionX.record = '.$modx->toJSON($v).'; </script>
+                    <style type="text/css">
+                        .ext-gecko .x-form-text, .ext-ie8 .x-form-text {padding-top: 0;}
+                        .vx-added .x-form-item-label { color: green; } .vx-changed .x-form-item-label { color: #dd6600; } .vx-removed .x-form-item-label { color: #ff0000; }
+                    </style>
+                ');
+        }
+        /* If an ID to compare to was passed, fetch that aswell. */
+        if (intval($_REQUEST['cmid']) > 0) {
+            $v = $versionx->getVersionDetails('vxTemplateVar',intval($_REQUEST['cmid']));
+            if ($v !== false)
+            {
+                $modx->regClientStartupHTMLBlock('<script type="text/javascript">VersionX.cmrecord = '.$modx->toJSON($v).'; </script>');
+            }
+        }
+
+        $modx->regClientStartupScript($versionx->config['js_url'].'mgr/action.templatevar.js');
+        $modx->regClientStartupScript($versionx->config['js_url'].'mgr/common/panel.common.js');
+        $modx->regClientStartupScript($versionx->config['js_url'].'mgr/common/grid.common.js');
+        $modx->regClientStartupScript($versionx->config['js_url'].'mgr/templatevars/detailpanel.templatevars.js');
+        $modx->regClientStartupScript($versionx->config['js_url'].'mgr/templatevars/combo.versions.templatevars.js'); 
+    break;
+    
     case 'index':
     default:
         $modx->regClientStartupScript($versionx->config['js_url'].'mgr/action.index.js');
