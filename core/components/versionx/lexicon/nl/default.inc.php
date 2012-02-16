78ab4eb8d522dc334ada968e624e775d007cea83
Engelbert Mercelis <engelbertmercelis@gmail.com>
2012-02-16 23:25:34 +0100

    EB1: Translated to dutch
    Needs a review, since i never used modx in dutch i do not know all the right terms.

---
+++ core/components/versionx/lexicon/nl/default.inc.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * VersionX
+ *
+ * Copyright 2011 by Mark Hamstra <hello@markhamstra.com>
+ *
+ * VersionX is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * VersionX is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * VersionX; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package versionx
+ * @subpackage lexicon-nl
+ * @author Engelbert Mercelis 
+ * @date 2012-02-16
+ *
+ */
+
+$_lang['versionx'] = 'VersionX';
+$_lang['versionx.menu_desc'] = 'Een eenvoudig revisie systeem.';
+
+$_lang['versionx.home'] = 'VersionX';
+$_lang['versionx.home.text'] = 'VersionX is een hulpprogramma voor MODX Revolution dat u zal helpen met het bijhouden van uw inhoud in Resources, Templates, Chunks, Snippets en Plugins. Elke keer u iets opslaat wordt dit geregistreerd en kan eenvoudig worden bekeken en vergeleken met deze component. Houd er rekening mee dat, hoewel de interface voor Chunks, Snippets en Plugins nog niet zijn opgenomen, zij toch worden geregistreerd en opgeslagen in de database voor toekomstig gebruik.';
+
+$_lang['versionx.resources.detail'] = 'Resource Details';
+$_lang['versionx.resources.detail.text'] = 'Hieronder vindt u de gegevens van het Resource Versie die u hebt geselecteerd. Om deze Versie te vergelijken met een andere, gebruik onderstaande combobox om een andere Versie te kiezen.';
+
+$_lang['versionx.resources.detail.tabs.version-details'] = 'Versie Details';
+$_lang['versionx.resources.detail.tabs.resource-fields'] = 'Velden';
+$_lang['versionx.resources.detail.tabs.resource-content'] = 'Inhoud';
+$_lang['versionx.resources.detail.tabs.resource-content.columnheader'] = 'Inhoud voor Versie #[[+id]]';
+$_lang['versionx.resources.detail.tabs.template-variables'] = 'Template Variabelen';
+$_lang['versionx.resources.detail.tabs.page-settings'] = 'Pagina Instellingen';
+
+$_lang['versionx.resources.detail.grid.columns.field-name'] = 'Veld Naam';
+$_lang['versionx.resources.detail.grid.columns.field-value'] = 'Veld Waarde [Versie #[[+id]]]';
+
+$_lang['versionx.templates.detail.tabs.version-details'] = 'Versie Details';
+$_lang['versionx.templates.detail.tabs.fields'] = 'Velden';
+$_lang['versionx.templates.detail.tabs.content'] = 'Inhoud';
+$_lang['versionx.templates.detail.tabs.properties'] = 'Eigenschappen';
+$_lang['versionx.templates.detail.tabs.properties.off'] = 'Sorry, we kunnen dit tabblad nog niet laten zien';
+
+$_lang['versionx.templates.detail'] = 'Template Details';
+$_lang['versionx.templates.detail.text'] = 'Hieronder vindt u de gegevens van de Template Versie die u hebt geselecteerd. Om deze Versie te vergelijken met een andere, gebruik onderstaande combobox om een andere Versie te kiezen.';
+
+$_lang['versionx.menu.viewdetails'] = 'Bekijk Versie Details';
+$_lang['versionx.back'] = 'Terug naar het Overzicht';
+$_lang['versionx.backtoresource'] = 'Terug naar de Resource';
+$_lang['versionx.compare_to'] = 'Vergelijk met';
+$_lang['versionx.compare_this_version_to'] = 'Vergelijk deze Versie met';
+$_lang['versionx.filter'] = 'Filter [[+what]]';
+$_lang['versionx.filter.reset'] = 'Reset Filter';
+$_lang['versionx.filter.datefrom'] = 'Vanaf';
+$_lang['versionx.filter.dateuntil'] = 'Tot';
+
+$_lang['versionx.version_id'] = 'Versiz ID';
+$_lang['versionx.content_id'] = '[[+what]] ID';
+$_lang['versionx.content_name'] = '[[+what]] Naam';
+$_lang['versionx.mode'] = 'Mode';
+$_lang['versionx.mode.new'] = 'Maak';
+$_lang['versionx.mode.upd'] = 'Bijwerken';
+$_lang['versionx.mode.snapshot'] = 'Snapshot';
+$_lang['versionx.saved'] = 'Opgeslagen Op';
+$_lang['versionx.title'] = 'Titel';
+$_lang['versionx.marked'] = 'Gemarkeerd';
+
+$_lang['versionx.error.noresults'] = 'Geen resultaten gevonden voor uw zoekopdracht.';
+$_lang['versionx.tabtip.notyet'] = 'Sorry, we kunnen uw [[+ wat]] geschiedenis nog niet laten zien. Wees gerust veranderingen worden nu al gecontroleerd, en een keer we dit tabblad toevoegen ben je klaar voor de actie!';
+
+?>
