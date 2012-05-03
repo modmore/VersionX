68e88bb3d0913a40db5e48f8db360d16bb896018
Mark Hamstra <hello@markhamstra.com>
2012-05-03 02:49:56 +0200

    Prettify setup options in 2.2+

--- _build/setup.options.php
+++ _build/setup.options.php
@@ -15,17 +15,17 @@ $output = <<<HTML
 <p>Choose what to create snapshots for:</p>
 <div style="padding-left: 20px;">
     <input type="checkbox" name="vx_snapshot_resources" id="vx_snapshot_resources" />
-        <label for="vx_snapshot_resources">Resources</label> <br />
+        <label for="vx_snapshot_resources" style="display: inline;">Resources</label> <br />
     <input type="checkbox" name="vx_snapshot_templates" id="vx_snapshot_templates" />
-        <label for="vx_snapshot_templates">Templates</label> <br />
+        <label for="vx_snapshot_templates" style="display: inline;">Templates</label> <br />
     <input type="checkbox" name="vx_snapshot_chunks" id="vx_snapshot_chunks" />
-        <label for="vx_snapshot_chunks">Chunks</label> <br />
+        <label for="vx_snapshot_chunks" style="display: inline;">Chunks</label> <br />
     <input type="checkbox" name="vx_snapshot_snippets" id="vx_snapshot_snippets" />
-        <label for="vx_snapshot_snippets">Snippets</label> <br />
+        <label for="vx_snapshot_snippets" style="display: inline;">Snippets</label> <br />
     <input type="checkbox" name="vx_snapshot_plugins" id="vx_snapshot_plugins" />
-        <label for="vx_snapshot_plugins">Plugins</label> <br />
+        <label for="vx_snapshot_plugins" style="display: inline;">Plugins</label> <br />
     <input type="checkbox" name="vx_snapshot_tmplvars" id="vx_snapshot_tmplvars" />
-        <label for="vx_snapshot_tmplvars">Template Variables</label>
+        <label for="vx_snapshot_tmplvars" style="display: inline;">Template Variables</label>
 </div>
 HTML;
 
