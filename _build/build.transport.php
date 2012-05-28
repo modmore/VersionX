94b1e94096a99fde33dc5f156c4da351519b27f0
Mark Hamstra <hello@markhamstra.com>
2012-05-29 00:03:29 +0200

    Bump to 2.0.0-rc2.

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -23,7 +23,7 @@ set_time_limit(0);
 define('PKG_NAME','VersionX');
 define('PKG_NAME_LOWER',strtolower(PKG_NAME));
 define('PKG_VERSION','2.0.0');
-define('PKG_RELEASE','rc1');
+define('PKG_RELEASE','rc2');
 
 $root = dirname(dirname(__FILE__)).'/';
 $sources= array (
