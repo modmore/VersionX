ba8ce2fa4e0855dd725d3b40814c638cd28eb187
Mark Hamstra <hello@markhamstra.com>
2012-07-08 22:26:26 +0200

    Updated German lexicon

--- core/components/versionx/lexicon/de/default.inc.php
+++ core/components/versionx/lexicon/de/default.inc.php
@@ -26,14 +26,17 @@ $_lang['versionx'] = 'VersionX';
 $_lang['versionx.tabheader'] = 'Versionen';
 $_lang['versionx.menu_desc'] = 'Sichert Ihren wertvollen Content.';
 
-$_lang['versionx.common.empty'] = '&lt;leer&gt;';
-$_lang['versionx.common.version-details'] = 'Version Details';
-$_lang['versionx.common.detail.text'] = 'Unten finden Sie die Details für die [[+what]] Version, die Sie ausgewählt haben. Um diese Version mit einer anderen zu vergleichen, wählen Sie über die Combobox unten eine andere Version aus.';
-
 $_lang['versionx.home'] = 'VersionX';
 $_lang['versionx.home.text'] = 'VersionX ist ein nützliches Werkzeug für MODX Revolution, das Ihnen hilft den Überblick über Ihre Inhalte in Ressourcen, Templates, Chunks, Snippets und Plugins zu behalten. Jeder Speichervorgang wird aufgezeichnet und kann leicht verglichen und wiederhergestellt bzw. rückgängig gemacht werden. Bitte beachten Sie, dass die Schnittstelle für Chunks, Snippets und Plugins noch nicht enthalten sind, die Daten werden in der Datenbank gespeichert und stehen zur Verfügung, sobald die Funktionen implementiert sind. <br /> <br />
 	VersionX steht kostenlos (und Open Source) zur Verfügung, jedoch wurde eine Menge Zeit in Entwicklung, Wartung und Support investiert. Bitte <a href="http://www.markhamstra.com/open-source/versionx/">denken Sie über eine Spende nach</a> und unterstütze die zukünftige Weiterentwicklung von VersionX';
 
+$_lang['versionx.common.empty'] = '&lt;leer&gt;';
+$_lang['versionx.common.version-details'] = 'Version Details';
+$_lang['versionx.common.detail.text'] = 'Unten finden Sie die Details für die [[+what]] Version, die Sie ausgewählt haben. Um diese Version mit einer anderen zu vergleichen, wählen Sie über die Combobox unten eine andere Version aus.';
+$_lang['versionx.common.fields'] = 'Felder';
+$_lang['versionx.common.content'] = 'Content';
+$_lang['versionx.common.properties'] = 'Eigenschaften';
+$_lang['versionx.common.properties.off'] = 'Sorry, dieses Tab kann noch nicht angezeigt werden.';
 
 $_lang['versionx.resources.detail'] = 'Ressourcendetails';
 $_lang['versionx.resources.detail.text'] = 'Unten finden Sie Details zur ausgewählten Ressourcenversion. Um diese Version mit einer anderen zu vergleichen, wählen Sie über die Combobox unten eine andere Version aus.';
@@ -43,8 +46,6 @@ $_lang['versionx.resources.revert.confirm'] = 'Sind Sie sicher?';
 $_lang['versionx.resources.revert.confirm.text'] = 'Sind Sie sicher, dass Sie zur Version #[[+id]] zurückkehren wollen? Dies überschreibt ALLE Felder und Template Variablen, die momentan für die Ressource festgelegt sind und ersetzt die Werte mit denjenigen in der ausgewählten Ressourcenversion.';
 $_lang['versionx.resources.reverted'] = 'Ressourcenversion erfolgreich wiederhergestellt!';
 
-$_lang['versionx.resources.detail.tabs.resource-fields'] = 'Felder';
-$_lang['versionx.resources.detail.tabs.resource-content'] = 'Content';
 $_lang['versionx.resources.detail.tabs.resource-content.columnheader'] = 'Content für Version #[[+id]]';
 $_lang['versionx.resources.detail.tabs.template-variables'] = 'Template Variablen';
 $_lang['versionx.resources.detail.tabs.page-settings'] = 'Seiteneinstellungen';
@@ -52,17 +53,11 @@ $_lang['versionx.resources.detail.tabs.page-settings'] = 'Seiteneinstellungen';
 $_lang['versionx.resources.detail.grid.columns.field-name'] = 'Feldname';
 $_lang['versionx.resources.detail.grid.columns.field-value'] = 'Feldinhalt [Ver #[[+id]]]';
 
-$_lang['versionx.templates.detail.tabs.fields'] = 'Felder';
-$_lang['versionx.templates.detail.tabs.content'] = 'Content';
-$_lang['versionx.templates.detail.tabs.properties'] = 'Eigenschaften';
-$_lang['versionx.templates.detail.tabs.properties.off'] = 'Sorry, dieses Tab kann noch nicht angezeigt werden.';
 $_lang['versionx.templates.detail'] = 'Template Details';
 $_lang['versionx.templates.detail.text'] = 'Unten finden Sie Details zur ausgewählten Template-Version. Um diese Version mit einer anderen zu vergleichen, wählen Sie über die Combobox unten eine andere Version aus.';
 
 $_lang['versionx.templatevars.detail.tabs.input-options'] = 'Eingabe-Optionen';
 $_lang['versionx.templatevars.detail.tabs.output-options'] = 'Ausgabe-Optionen';
-$_lang['versionx.templatevars.detail.tabs.properties'] = 'Eigenschaften';
-$_lang['versionx.templatevars.detail.tabs.properties.off'] = 'Sorry, dieses Tab kann noch nicht angezeigt werden.';
 
 $_lang['versionx.templatevars.detail'] = 'Template Variable Details';
 $_lang['versionx.templatevars.detail.input-type'] = 'Eingabetyp';
@@ -71,13 +66,14 @@ $_lang['versionx.templatevars.detail.default-text'] = 'Standardwert';
 $_lang['versionx.templatevars.detail.output-type'] = 'Ausgabetyp';
 $_lang['versionx.templatevars.detail.output-properties'] = 'Ausgabe-Optionswerte';
 
-$_lang['versionx.chunks.detail.tabs.fields'] = 'Felder';
-$_lang['versionx.chunks.detail.tabs.content'] = 'Content';
-$_lang['versionx.chunks.detail.tabs.properties'] = 'Eigenschaften';
-$_lang['versionx.chunks.detail.tabs.properties.off'] = 'Sorry, dieses Tab kann noch nicht angezeigt werden.';
-$_lang['versionx.chunks.detail'] = 'Template Details';
+$_lang['versionx.chunks.detail'] = 'Chunk Details';
 $_lang['versionx.chunks.detail.text'] = 'Unten finden Sie Details zur ausgewählten Chunk-Version. Um diese Version mit einer anderen zu vergleichen, wählen Sie über die Combobox unten eine andere Version aus.';
 
+$_lang['versionx.snippets.detail'] = 'Snippet Details';
+$_lang['versionx.snippets.detail.text'] = 'Unten finden Sie Details zur ausgewählten Snippet-Version. Um diese Version mit einer anderen zu vergleichen, wählen Sie über die Combobox unten eine andere Version aus.';
+
+$_lang['versionx.plugins.detail'] = 'Plugin Details';
+$_lang['versionx.plugins.detail.text'] = 'Unten finden Sie Details zur ausgewählten Plugin-Version. Um diese Version mit einer anderen zu vergleichen, wählen Sie über die Combobox unten eine andere Version aus.';
 
 $_lang['versionx.menu.viewdetails'] = 'Version Details ansehen';
 $_lang['versionx.back'] = 'Zurück zur Übersicht';
@@ -103,3 +99,8 @@ $_lang['versionx.marked'] = 'Markiert';
 
 $_lang['versionx.error.noresults'] = 'Für Ihre Anfrage konnten keine Resultate gefunden werden.';
 $_lang['versionx.tabtip.notyet'] = 'Sorry, leider können wir die History für [[+what]] noch nicht anzeigen. Veränderungen werden jedoch bereits jetzt in die Datenbank geschrieben, sobald diese Funktionen implementiert sind, werden sie hier angezeigt!';
+
+/* @todo 08/07/2012 translate these */
+$_lang['versionx.widget.resources'] = 'Recent Resource Changes';
+$_lang['versionx.widget.resources.desc'] = '(Part of VersionX) Shows a grid with the most recent resource changes for all users.';
+$_lang['versionx.widget.resources.update'] = 'Update Resource';
