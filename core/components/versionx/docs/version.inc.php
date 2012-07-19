932e4b5f0e69618a2529450ecf4e2f6731de3729
Mark Hamstra <hello@markhamstra.com>
2012-07-19 15:41:46 +0200

    rc4 patch release: Fix critical issue on Firefox (possibly others too) affecting TVs.

--- core/components/versionx/docs/version.inc.php
+++ core/components/versionx/docs/version.inc.php
@@ -1,5 +1,5 @@
 <?php
 
 define('VERSIONX_VERSION','2.0.0');
-define('VERSIONX_RELEASE','rc3');
+define('VERSIONX_RELEASE','rc4');
 define('VERSIONX_FULLVERSION',VERSIONX_VERSION . '-' . VERSIONX_RELEASE);
