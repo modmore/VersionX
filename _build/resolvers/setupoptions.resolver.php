46860863f9d2f68682ebf9915a3ac2006bba80a9
Mark Hamstra <hello@markhamstra.com>
2012-12-29 02:30:08 +0100

    #43 Improve setup process performance by using getIterator instead of getCollection.

--- _build/resolvers/setupoptions.resolver.php
+++ _build/resolvers/setupoptions.resolver.php
@@ -25,92 +25,81 @@ switch ($options[xPDOTransport::PACKAGE_ACTION]) {
         $modx->log(modX::LOG_LEVEL_INFO,'Starting snapshot process for selected objects...');
         /* If we wanted resources snapshot, make that */
         if (isset($options['vx_snapshot_resources']) && !empty($options['vx_snapshot_resources'])) {
-            $collection = $modx->getCollection('modResource');
-            $modx->log(modX::LOG_LEVEL_INFO,'Resources loaded. Iterating over resources and storing snapshots..');
+            $modx->log(modX::LOG_LEVEL_INFO,'Iterating over Resources and storing snapshots..');
             $count = 0;
-            foreach ($collection as $object) {
-                if ($versionx->newResourceVersion($object, 'snapshot')) {
-                    $count++;
-                } else {
-                    $modx->log(modX::LOG_LEVEL_WARN,'Error creating snapshot for resource '.$object->get('id'));
-                }
-                if (is_int($count / 25)) {
-                    $modx->log(modX::LOG_LEVEL_INFO,"Checked {$count} resources so far.");
-                }
+            foreach ($modx->getIterator('modResource') as $object) {
+                if ($versionx->newResourceVersion($object, 'snapshot')) { $count++; }
+                else { $modx->log(modX::LOG_LEVEL_WARN,'Error creating snapshot for Resource '.$object->get('id')); }
+
+                if (is_int($count / 25)) { $modx->log(modX::LOG_LEVEL_INFO,"Checked {$count} Resources so far."); }
             }
-            $modx->log(modX::LOG_LEVEL_WARN,'Checked '.$count.' resources and stored snapshots where needed.');
+            $modx->log(modX::LOG_LEVEL_WARN,'Iterated over '.$count.' Resources.');
         }
 
         /* If we wanted template snapshot, make that */
         if (isset($options['vx_snapshot_templates']) && !empty($options['vx_snapshot_templates'])) {
-            $collection = $modx->getCollection('modTemplate');
-            $modx->log(modX::LOG_LEVEL_INFO,'Templates loaded. Iterating over templates and storing snapshots..');
+            $modx->log(modX::LOG_LEVEL_INFO,'Iterating over Templates and storing snapshots..');
             $count = 0;
-            foreach ($collection as $object) {
+            foreach ($modx->getIterator('modTemplate') as $object) {
                 if ($versionx->newTemplateVersion($object, 'snapshot')) { $count++; }
-                else { $modx->log(modX::LOG_LEVEL_WARN,'Error creating snapshot for template '.$object->get('id')); }
+                else { $modx->log(modX::LOG_LEVEL_WARN,'Error creating snapshot for Template '.$object->get('id')); }
 
-                if (is_int($count / 10)) { $modx->log(modX::LOG_LEVEL_INFO,"Checked {$count} templates so far."); }
+                if (is_int($count / 10)) { $modx->log(modX::LOG_LEVEL_INFO,"Checked {$count} Templates so far."); }
             }
-            $modx->log(modX::LOG_LEVEL_WARN,'Checked '.$count.' Templates and stored snapshots where needed.');
+            $modx->log(modX::LOG_LEVEL_WARN,'Iterated over  '.$count.' Templates.');
         }
 
         /* If we wanted chunk snapshot, make that */
         if (isset($options['vx_snapshot_chunks']) && !empty($options['vx_snapshot_chunks'])) {
-            $collection = $modx->getCollection('modChunk');
-            $modx->log(modX::LOG_LEVEL_INFO,'Chunks loaded. Iterating over chunks and storing snapshots..');
+            $modx->log(modX::LOG_LEVEL_INFO,'Iterating over Chunks and storing snapshots..');
             $count = 0;
-            foreach ($collection as $object) {
+            foreach ($modx->getIterator('modChunk') as $object) {
                 if ($versionx->newChunkVersion($object, 'snapshot')) { $count++; }
-                else { $modx->log(modX::LOG_LEVEL_WARN,'Error creating snapshot for chunk '.$object->get('id')); }
+                else { $modx->log(modX::LOG_LEVEL_WARN,'Error creating snapshot for Chunk '.$object->get('id')); }
 
-                if (is_int($count / 25)) { $modx->log(modX::LOG_LEVEL_INFO,"Checked {$count} chunks so far."); }
+                if (is_int($count / 25)) { $modx->log(modX::LOG_LEVEL_INFO,"Checked {$count} Chunks so far."); }
             }
-            $modx->log(modX::LOG_LEVEL_WARN,'Checked '.$count.' Chunks and stored snapshots where needed.');
+            $modx->log(modX::LOG_LEVEL_WARN,'Iterated over '.$count.' Chunks.');
         }
 
         /* If we wanted snippet snapshot, make that */
         if (isset($options['vx_snapshot_snippets']) && !empty($options['vx_snapshot_snippets'])) {
-            $collection = $modx->getCollection('modSnippet');
-            $modx->log(modX::LOG_LEVEL_INFO,'Snippets loaded. Iterating over snippets and storing snapshots..');
+            $modx->log(modX::LOG_LEVEL_INFO,'Iterating over Snippets and storing snapshots..');
             $count = 0;
-            foreach ($collection as $object) {
+            foreach ($modx->getIterator('modSnippet') as $object) {
                 if ($versionx->newSnippetVersion($object, 'snapshot')) { $count++; }
                 else { $modx->log(modX::LOG_LEVEL_WARN,'Error creating snapshot for Snippet '.$object->get('id')); }
 
-                if (is_int($count / 10)) { $modx->log(modX::LOG_LEVEL_INFO,"Checked {$count} snippets so far."); }
+                if (is_int($count / 10)) { $modx->log(modX::LOG_LEVEL_INFO,"Checked {$count} Snippets so far."); }
             }
-            $modx->log(modX::LOG_LEVEL_WARN,'Checked '.$count.' Snippets and stored snapshots where needed.');
+            $modx->log(modX::LOG_LEVEL_WARN,'Iterated over '.$count.' Snippets.');
         }
 
         /* If we wanted plugins snapshot, make that */
         if (isset($options['vx_snapshot_plugins']) && !empty($options['vx_snapshot_plugins'])) {
-            $collection = $modx->getCollection('modPlugin');
-            $modx->log(modX::LOG_LEVEL_INFO,'Plugins loaded. Iterating over Plugins and storing snapshots..');
+            $modx->log(modX::LOG_LEVEL_INFO,'Iterating over Plugins and storing snapshots..');
             $count = 0;
-            foreach ($collection as $object) {
+            foreach ($modx->getIterator('modPlugin') as $object) {
                 if ($versionx->newPluginVersion($object, 'snapshot')) { $count++; }
                 else { $modx->log(modX::LOG_LEVEL_WARN,'Error creating snapshot for Plugin '.$object->get('id')); }
 
                 if (is_int($count / 10)) { $modx->log(modX::LOG_LEVEL_INFO,"Checked {$count} Plugins so far."); }
             }
-            $modx->log(modX::LOG_LEVEL_WARN,'Checked '.$count.' Plugins and stored snapshots where needed.');
+            $modx->log(modX::LOG_LEVEL_WARN,'Iterated over '.$count.' Plugins.');
         }
 
         /* If we wanted tv snapshot, make that */
         if (isset($options['vx_snapshot_tmplvars']) && !empty($options['vx_snapshot_tmplvars'])) {
-            $collection = $modx->getCollection('modTemplateVar');
-            $modx->log(modX::LOG_LEVEL_INFO,'Template Variables loaded. Iterating over Template Variables and storing snapshots..');
+            $modx->log(modX::LOG_LEVEL_INFO,'Iterating over Template Variables and storing snapshots..');
             $count = 0;
-            foreach ($collection as $object) {
+            foreach ($modx->getIterator('modTemplateVar') as $object) {
                 if ($versionx->newTemplateVarVersion($object, 'snapshot')) { $count++; }
                 else { $modx->log(modX::LOG_LEVEL_WARN,'Error creating snapshot for Template Variable '.$object->get('id')); }
 
                 if (is_int($count / 10)) { $modx->log(modX::LOG_LEVEL_INFO,"Checked {$count} Template Variables so far."); }
             }
-            $modx->log(modX::LOG_LEVEL_WARN,'Checked '.$count.' Template Variables and stored snapshots where needed.');
+            $modx->log(modX::LOG_LEVEL_WARN,'Iterated over '.$count.' Template Variables.');
         }
-
     break;
 }
     
