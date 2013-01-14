05b34675ff2f8612f4c1bdc2559a7843ae578485
Mark Hamstra <hello@markhamstra.com>
2013-01-14 13:14:48 +0100

    Bump

--- core/components/versionx/docs/version.inc.php
+++ core/components/versionx/docs/version.inc.php
@@ -1,5 +1,5 @@
 <?php
 
-define('VERSIONX_VERSION','2.0.0');
+define('VERSIONX_VERSION','2.1.0');
 define('VERSIONX_RELEASE','pl');
 define('VERSIONX_FULLVERSION',VERSIONX_VERSION . '-' . VERSIONX_RELEASE);
