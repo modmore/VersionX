bfc25c25e02fbfda49f402b1fc806ac19e52f6cb
Mark Hamstra <hello@markhamstra.com>
2015-05-15 16:59:55 +0200

    - #59 Fix E_NOTICE issue in PHP < 5.4

--- config.core.php
+++ config.core.php
@@ -1,3 +1,3 @@
 <?php
-define('MODX_CORE_PATH', '/Applications/MAMP/htdocs/modx-stable2/core/');
+define('MODX_CORE_PATH', '/Applications/MAMP/htdocs/release-2.3/core/');
 define('MODX_CONFIG_KEY', 'config');
