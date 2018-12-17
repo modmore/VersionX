977aced0d18542f14d2cd018d9e9037ea56a488b
Mark Hamstra <hello@markhamstra.com>
2018-12-17 13:10:07 +0100

    2.2.1

--- core/components/versionx/docs/version.inc.php
+++ core/components/versionx/docs/version.inc.php
@@ -1,5 +1,5 @@
 <?php
 
-define('VERSIONX_VERSION','2.2.0');
+define('VERSIONX_VERSION','2.2.1');
 define('VERSIONX_RELEASE','pl');
 define('VERSIONX_FULLVERSION',VERSIONX_VERSION . '-' . VERSIONX_RELEASE);
