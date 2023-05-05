8c76a92e97c997b945562c2d19f7c49ef7da566b
Murray <murray@digitalpenguin.hk>
2023-05-05 13:27:52 +0800

    Update setup options display, include note about migration command line script. Warn this is a beta version. Start on readme.

--- _build/setup.options.php
+++ _build/setup.options.php
@@ -5,10 +5,42 @@ switch ($options[xPDOTransport::PACKAGE_ACTION]) {
     case xPDOTransport::ACTION_UPGRADE:
 
 $output = <<<HTML
+<style>
+    .versionx-header {
+        display: flex;
+        align-items: center;
+        gap: 8px;
+        margin-top: -30px;
+    }
+    .versionx-header .version {
+        position: relative;
+        bottom: 15px;
+        right: 5px;
+        color: #fff;
+        background: #ff9640 !important;
+        border-radius: 50%;
+        width: 25px;
+        height: 25px;
+        text-align: center;
+        vertical-align: center;
+        line-height: 24px;
+        font-size: 20px;
+        box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.15);
+    }
+</style>
+<div class="versionx-header">
+    <h2>VersionX</h2>
+    <div class="version">3</div>
+</div>
+<p style="font-size: 90%; font-style: italic;">Resource and element versioning for MODX</p>
+<br>
+<p style="font-weight: bold;">Migrating from version 2.x</p>
+<p>VersionX 3 is a complete rewrite from version 2 and a command line migration script is included to migrate your data to the new version. After installing, from the command line, run: <br><br><pre style="background: #f8fafc; padding: 5px 15px; border-radius: 3px;">php core/components/versionx/migrate.php</pre></p>
+<br>
 <p>If you already have resources and elements in place, you can let the setup process create a snapshot of them to the VersionX tables, providing you with a baseline to start comparing from. The system will check for existing versions and only create a snapshot when needed.</p>
-<br />
+<br>
 <p style="font-size: 90%; font-style: italic;">Note: depending on the amount of resources and elements you currently have in place as well as your server's configuration, this process can take quite some time.</p>
-<br />
+<br>
 <p>Choose what to create snapshots for:</p>
 <div style="padding-left: 20px;">
     <input type="checkbox" name="vx_snapshot_resources" id="vx_snapshot_resources" />
@@ -25,7 +57,10 @@ $output = <<<HTML
         <label for="vx_snapshot_tmplvars" style="display: inline;">Template Variables</label>
 </div>
 
-<p>If you found VersionX useful, please consider <a href="https://modmore.com/extras/versionx/donate/">making a donation to support its development</a>. Thank you!</p>
+<p style="margin-top: 15px;">If you found VersionX useful, please consider <a href="https://modmore.com/extras/versionx/donate/">making a donation to support its development</a>. Thank you!</p>
+<br>
+<p style="background: red; color: white; font-weight: bold; padding: 5px 15px;">This version is still in beta and is not yet ready for production environments. Only install this on test environments until the stable version is released!</p>
+
 HTML;
     break;
     default:
---
+++ readme.md
@@ -0,0 +1,25 @@
+# VersionX 3 #
+
+_If you're looking for the current stable version of VersionX, see the [v2 branch](https://github.com/modmore/VersionX/tree/master)._
+
+VersionX is a versioning system for the MODX content management system. It keeps track of changes to your resources, templates, template variables, chunks, snippets, plugins, and _now in v3 custom objects too!_ You can view historic changes and revert changes from a simple dashboard.
+
+![VersionX Screenshot](https://user-images.githubusercontent.com/5160368/236378160-f0e88939-71e4-4d38-b750-f49ae84b068d.png)
+
+v3.0 is a complete rewrite and is not compatible with v2. 
+There is, however, a migration utility included that will assist with moving your old data to the new tables.
+
+## The Road to v3.0 ##
+Version 3 would not have been possible if not for the impressive community funding. _[More on this soon...]_
+
+## Requirements ##
+
+- MODX 2.6.5+ 
+- PHP 7.4+
+
+## Installation ## 
+
+VersionX can be installed from either the [modmore package provider](https://modmore.com/about/package-provider/), or the [MODX package provider](https://extras.modx.com/package/versionx?version=2.4.0-pl).
+
+## Contributions ##
+VersionX is open source, and free to use. If you would like to support further development, feel free to contribute with either code (in the form of a PR on this repo) or monetary contributions. Hit the link to donate: https://modmore.com/extras/versionx/donate/ 
