294380170c884a10a2b48c97ffbd52c32bc94502
Mark Hamstra <hello@markhamstra.com>
2023-08-02 18:02:35 +0200

    Install test 2

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -49,9 +49,6 @@ $sources = [
 ];
 unset($root);
 
-require_once $sources['root'] . 'config.core.php';
-require_once MODX_CORE_PATH . 'model/modx/modx.class.php';
-
 $modx= new modX();
 $modx->initialize('mgr');
 $modx->setLogLevel(modX::LOG_LEVEL_INFO);
